beafe2fd317bd1fe9b0c7a40aace25e7b09ee689
Hasan Zagarevic <hasanzagarevic2019@gmail.com>
2023-10-15 18:11:06 +0200

    nesto

---
+++ app.js
@@ -0,0 +1,559 @@
+//if (h <= 23 && h >= 0 && m <= 59 && s >= 0 && s <=59 ){
+//    return s*1000 + m * 60 * 1000 + h * 60 * 60 * 1000}
+
+
+// kurs osnovi programiranja
+// kurs o mrezama
+//------------------------------------------
+//let broj = parseInt(prompt("Unesi broj"))
+
+//if(broj % 2 === 0){
+    //console.log("paran");}
+//else{
+   // console.log("neparan");}
+// -------------------------------------------
+// let broj = parseInt(prompt("unesi broj"))
+
+// if(broj > 0)
+// {
+//     console.log("pozitivan");
+// }
+// else if (broj === 0){
+//     console.log("jednak nuli");
+// }
+// else{
+//     console.log("negativan");
+// }
+// -----------------------------------------
+
+// let broj1 = parseInt(prompt("broj 1"))
+// let broj2 = parseInt(prompt("broj 2"))
+
+// if (broj1 > broj2){
+//     console.log("broj 1 je veci");
+// }
+
+// else if (broj2 > broj1){
+//     console.log("veci je br 2");
+// }
+
+// else {
+//     console.log("isti");
+// }
+// -------------------------------------------
+
+// let broj1 = parseInt(prompt("broj1"))
+// let broj2 = parseInt(prompt("broj2"))
+// let broj3 = parseInt(prompt("broj3"))
+
+// if (broj3 ** 2 > broj1 && broj3 ** 2 < broj2){
+//     console.log("nalazi se izmedju");
+// }
+
+// else {
+//     console.log("ne nalazi se izmedju");
+// }
+// -----------------------------------------------
+
+
+
+// let x = parseFloat(prompt("unesi broj poena"))
+
+// if (x <= 50){
+//     console.log("ocena 5");
+// }
+
+// else if (x > 51 && x < 61){
+//     console.log("ocena 6");
+// }
+// else if (x > 61 && x < 70){
+//     console.log("ocena 7");
+// }
+
+// else if (x > 71 && x < 80){
+//     console.log("ocena 8");
+// }
+// else if (x > 81 && x < 90){
+//     console.log("ocena 9");
+// }
+// else if (x > 91 && x < 100){
+//     console.log("ocena 10");
+// }
+
+// ------------------------------------------------
+
+// let x = parseFloat(prompt("unesi broj1"));
+// let y = parseFloat(prompt("unesi broj2"));
+// let rezultat ;
+
+// if (x > y){
+// rezultat = x - y;
+// }
+
+// else {
+//     rezultat = y - x;
+// }
+// console.log(rezultat);
+// ------------------------------------------------------
+
+// let a = parseFloat(prompt("unesi broj1"));
+// let b = parseFloat(prompt("unesi broj2"));
+// let c = parseFloat(prompt("unesi broj3"));
+// var s,p;
+
+// if (a + b > c && a+c>b && b+c>a);{
+// s = (a+b+c)/2;
+// p= Math.sqrt(s * (s-a) * (s-b) * (s-c))
+// console.log(p);
+// }
+
+// else {
+//     console.log("nije moguce napraviti trougao");
+// }
+// ----------------------------------------------------------------------
+
+// for (let i = 1; i<=10; i++)
+// {
+//     console.log(i);
+// }
+// -------------------------------------------------------------------
+// for (let i = 100; i < 1000; i++){
+//     let prvaCifra = Math.floor(i / 100)
+// let drugaCifra = Math.floor((i % 100) / 10)
+// let trecaCifra = 1 % 10
+// if (drugaCifra === prvaCifra + 2 && trecaCifra === 1 + drugaCifra) {
+//     console.log(i);
+// }
+// }
+// -------------------------------------------------------------------------------
+// for (let i = 100; i < 1000; i==){
+//     let prvaCifra = Math.floor(i / 100)
+//     let drugaCifra = Math.floor((i % 100) / 10)
+//     let trecaCifra = i % 10
+//     if (Math.pow(prvaCifra, 3) + Math.pow(drugaCifra, 3) + Math.pow(trecaCifra, 3) === i){
+//         console.log(object);
+//     }
+// }
+// ---------------------------------------------------------------------
+// for (let a = 1; a <= 9; a== ){
+//     for( let b = 0; b<=9; b++){
+//         for(let c = 0; c<=9; c++){
+//             let broj = a*100 + b*10 + c
+
+//             if (broj % dveCifre === 0){
+//                 console.log(broj);
+//             }
+//         }
+//     }
+// }
+// -----------------------------------------------------------------------
+
+// let broj = parseInt(prompt("unesite broj: "))
+// console.log("delioci su: ");
+// for (let i = 1; i <= broj; i++)
+// if (broj % i i === 0){
+//     console.log(i);
+// }
+// -------------------------------------------------------------
+
+// let broj = parseInt(prompt("unesi br"))
+// console.log("delioci su");
+// let suma = 0;
+// for (let i = 1; i <= broj; i++)
+// {
+//     if (broj % i ===0 ){
+//         suma+=i
+//         console.log(suma);
+//     }
+
+// }
+// if (broj === suma){
+//     console.log("broj je savren");
+// }
+// else (broj )
+
+// --------------------------------------------------------------
+
+// let n = parseInt(prompt("unesite br"))
+// for ( let sat = 0; sat <= 23; sat++){
+
+// }
+
+// ----------------------------------------------------------------
+
+// let n = parseInt(prompt("unesi broj"))
+// for (let i = 0; i < n; i++){
+//     for (let j = 0; j < n; j++)
+//         console.log("*");
+//         console.log("\n");
+//     }
+// --------------------------------------------------------------
+
+// let n = parseInt(prompt("unesi broj"))
+// let m = parseInt(prompt("unesi broj"))
+// for (let i = 0; i < n; i++){
+// for (let j = 0; j < m; j++){
+//     console.log(j);
+// }
+// console.log(\n);
+// }
+
+// ------------------------------------------------------------------------
+
+// let n = parseInt(prompt("unesi broj"))
+// for (let i = 1; i < n; i++){
+//     for (let j = 0; j > n; j--)
+//     for (let k = )
+
+//     }
+// ---------------------------------------------------------------------
+// let n = parseInt(prompt("unesi br"))
+// let m = parseInt(prompt("unesi broj"))
+
+// for (let i = 0; i<n; i++){
+//     for (let j = 0; j<n; j++){
+//         console.log("*");
+//     }
+//     console.log("\n");
+// }
+
+// --------------------------------------------------------------------------
+
+// 
+
+
+// --------------------------------------------------------------------------------
+
+
+// let fp = 1; fpp = 1;
+// let n = parseInt(prompt("unesite broj"))
+// let i = 3;
+
+// while (i <= n){
+//     let 
+// }
+
+
+// -------------------------------------------------------------------------------------
+
+// let n = parseInt(prompt("unesite broj"))
+// let a = parseInt(prompt("unesite broj"))
+
+// let i = 0, proizvod = 1;
+// while (i<a){
+//     proizvod *= n
+//     i++
+// }
+// console.log(proizvod);
+
+// --------------------------------------------------------------------------------------------|
+
+
+// let suma = 0, Iterator = 0;
+
+// let stop = parseFloat(prompt("unesite broj"))
+// if stop >= 
+// while (stop !== 0){
+//     suma+= stop
+// stop = parseFloat(prompt("unesite broj"))
+// Iterator++
+// } 
+// console.log(suma / Iterator);
+
+// -----------------------------------------------------------------------------------------
+
+// let n = parseInt(prompt("unesite broj"))
+//  let suma = 0;
+
+// let pom = n;
+
+// while (pom >= 1){
+//     suma += pom % 10
+//     pom = Math.floor(pom/10)
+// }
+// if (n % suma === 0){
+//     console.log("deljivo");
+// }
+// else {
+//     console.log("nedeljivo");
+// }
+
+// ---------------------------------------------------------------------------------------------
+
+// let n = parseInt(prompt("unesite broj"))
+
+// let obrnuto = 0;
+// while (n > 0){
+//     obrnuto = obrnuto*10 + n%10
+//     n = Math.floor(n/10)
+// }
+// console.log(obrnuto);
+
+// ---------------------------------------------------------------------------------------------
+
+
+// let n = parseInt(prompt("unesite broj"))
+
+// for (let k = 2; k <= n; k++){
+//     while (n % k === 0){
+//     n /= k;
+//     console.log(k);
+//     }
+// }
+
+// ---------------------------------------------------------------------------------------------
+
+// for (let a = 1; a <= 20; a++){
+//     for(let b = 1; b <= 20; b++){
+//         let zbir = a*a + b*b
+//         let c=1;
+//         while(c*c <= zbir){
+//             if(c*c === zbir){
+//                 console.log(a. b, c);
+//             }
+//             c++
+//         }
+//     }
+// }
+// ----------------------------------------------------------------------------------------------
+
+
+// let = xa = 1, ya = 1, xb= 4, yb= 1, xc= 2, yc = 5;
+
+// let a= Math.sqrt(Math.pow(xb - xc, 2)+Math.pow(yb- yc, 2))
+// let b= Math.sqrt(Math.pow(xc - xa, 2)+Math.pow(yc- ya, 2))
+// let c= Math.sqrt(Math.pow(xa - xb, 2)+Math.pow(ya- yb, 2))
+
+// let s = (a+ b+ c)/2
+
+// let p = Math.sqrt(s* (s-a)*(s-b)*(s-c))
+
+// console.log(p);
+
+// ---------------------------------------------------------------------------------------------
+
+// let broj = parseInt(prompt("unesite broj"))
+
+// let a = Math.floor(broj/100)
+// let b = Math.floor(broj/10)%10
+// let c = Math.floor(broj%10)
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// console.log(a+b+c)
+
+// ---------------------------------------------------------
+
+// 500 100 1
+// 1267
+
+// let n = parseInt(prompt("unesite broj"))
+
+// let n500 = Math.floor(n/500)
+// let n100 = Math.floor((n%500)/100)
+// let n1 = Math.floor((n%500)%100)
+
+// console.log(n500, n100, n1);
+
+// ---------------------------------------------------------------
+
+// let n = parseInt(prompt("Unesite neparan broj:"))
+ 
+// let rowOutput = "+";
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+ 
+// console.log(rowOutput);
+// for(let j = 0; j < n-2; j++){
+//     rowOutput = ""
+//     for(let i = 1; i <= n; i++){
+//         if(i === 1 || i === n){
+//             rowOutput += "|"
+//         }
+//         else{
+//             rowOutput += " "
+//         }
+//     }
+//     console.log("");
+//     console.log(rowOutput);
+// }
+ 
+ 
+// rowOutput = "+";
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+// console.log(rowOutput);
+
+// --------------------------------------------
+
+
+
+
+// let n = parseInt(prompt("Unesite neparan broj:"))
+ 
+// let rowOutput = "+";
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+ 
+// console.log(rowOutput);
+
+// for(let j = 0; j < n-2; j++){
+//     rowOutput = ""
+//     for(let i = 1; i <= n; i++){
+//         if(i === 1 || i === n){
+//             rowOutput += "|"
+//         } 
+//         else{
+//             rowOutput += " "
+//         }
+//     }
+//     console.log("");
+//     console.log(rowOutput);
+// }
+ 
+// rowOutput = "+";
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+// console.log(rowOutput);
+
+
+
+// ----------------------------------------------------------
+
+// let n = parseInt(prompt("Unesite neparan broj:"))
+ 
+// let rowOutput = "+";
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+ 
+// console.log(rowOutput);
+// for(let j = 0; j < n-2; j++){
+//     rowOutput = ""
+//     for(let i = 1; i <= n; i++){
+//         if(i === 1 || i === n){
+//             rowOutput += "|"
+//         }
+//         else if(j + 2 === Math.ceil(n/2)){
+//             if(i === Math.ceil(n/2) && j + 2 === Math.ceil(n/2)){
+//                 rowOutput += "+"
+//             }
+//             else{
+//                 rowOutput += "-"
+//             }
+//         }
+//         else if (i === Math.ceil(n/2)){
+//             rowOutput += "|"
+//         }
+//         else{
+//             rowOutput += " "
+//         }
+//     }
+//     console.log("");
+//     console.log(rowOutput);
+// }
+ 
+ 
+// rowOutput = "+";
+ 
+// for(let i = 0; i < n-2; i++){
+//     rowOutput += "-"
+// }
+ 
+// rowOutput += "+"
+// console.log(rowOutput);
+
+// ----------------------------------------------------
+
+// let i=10
+
+// do{
+//     console.log(i);
+//     i--
+// }
+// while (i>=0)
+
+// -----------------------------------------------------
+
+// let i = 0
+
+// do {
+//     console.log(i);
+//     i+=2
+// }
+// while (i <= 10)
+
+// ----------------------------------------------------
+
+// let i
+// do{
+//     i = parseFloat(prompt("Unesite broj:"))
+// }
+// while (i*i>=100)
+
+// --------------------------------------------------- 6,4
+
+// let n= parseFloat(prompt("Unesite broj:"))
+// let stepen = 0
+// let broj = 2
+
+// do{
+//  console.log(Math.pow(broj, stepen));
+//  stepen++
+// }
+// while(Math.pow(broj, stepen)<=n)
+
+// ------------------------------------------------- 6.8
+
+// let x = parseFloat(prompt("Unesite d:"))
+// let y = parseFloat(prompt("Unesite g:"))
+// let broj = x;
+
+// do {
+//     if(x < 0 || y > 100){
+//         x = parseFloat(prompt("Unesite br:"));
+//         y = parseFloat(prompt("Unesite br:"));
+//         broj = x;
+//     }
+//     else{
+//         if (broj % 4 === 0){
+//             console.log(broj);
+//         }
+//         broj++;
+//     }
+// }
+// while (broj<=y);
+
+// ---------------------------------------------------
+
